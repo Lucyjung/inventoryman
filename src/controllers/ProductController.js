5be49569c952f3f1d13ecff30e8f8d8915039fb6
LUCY-LT\Nick <nickky23@hotmail.com>
2020-02-09 10:25:56 +0700

    add more function

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -7,7 +7,8 @@ module.exports = {
         res.json({success: true})
     },
     read: async(req, res) => {
-        res.json({success: true})
+        const prodList = await Product.listProduct()
+        res.json({success: true, result: prodList})
     },
     update: async(req, res) => {
         res.json({success: true})
--- src/models/Product.js
+++ src/models/Product.js
@@ -3,19 +3,27 @@ const {Datastore} = require('@google-cloud/datastore');
 // Creates a client
 const datastore = new Datastore();
 const kind = 'Product';
-async function addProduct(id, prodInfo) {
+
+module.exports = {
+  addProduct : async (id, prodInfo) => {
   
-  // The Cloud Datastore key for the new entity
-  const idKey = datastore.key([kind, id]);
+    // The Cloud Datastore key for the new entity
+    const idKey = datastore.key([kind, id]);
+  
+    // Prepares the new entity
+    const prod = {
+      key: idKey,
+      data: prodInfo,
+    };
+  
+    // Saves the entity
+    await datastore.save(prod);
+    console.log(`Saved ${prod.key.name}`);
+  },
+  listProduct : async () =>{ 
+    const query = datastore.createQuery(kind);
+    const [prods] = await datastore.runQuery(query);
 
-  // Prepares the new entity
-  const prod = {
-    key: idKey,
-    data: prodInfo,
-  };
-
-  // Saves the entity
-  await datastore.save(prod);
-  console.log(`Saved ${prod.key.name}`);
-}
-module.exports = {addProduct};
+    return prods;
+  }
+};
